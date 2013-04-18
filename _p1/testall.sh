962154aac78c6d95e5aca2053f4b08e6abb07bd4
Jon Larson <jonlarson90@gmail.com>
2013-04-18 11:33:01 -0700

    Adding project 2

---
+++ _p1/testall.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+make clean
+make
+
+rm -f samples/*.mine
+#cat /dev/null > out
+
+for file in `ls samples/*.frag samples/*.decaf`
+	do
+                filename=$(basename "$file")
+		extension="${filename##*.}"
+		testcasename="${filename%.*}"
+
+		my_output=samples/"$testcasename".mine
+#                frag=samples/"$testcasename".frag
+		their_output=samples/"$testcasename".out
+
+                dos2unix --quiet $file # converts files in place
+                dos2unix --quiet $their_output #use -n option to save separate
+
+		cat /dev/null > $my_output
+                ./dcc < $file &> $my_output
+
+		differences=$(diff ${my_output} ${their_output})
+
+		if [ -n "$differences" ]; then
+			echo
+			#echo "Difference found in testcase $testcasename ($my_output vs $their_output): "
+			#echo "$differences" | tee out
+			#echo
+			#echo "See the out file"
+      vimdiff $my_output $their_output
+			exit 1
+		fi
+	done
+
+echo "It seems to be okay"
